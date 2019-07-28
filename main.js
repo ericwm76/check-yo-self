ade9b6f0b12b4c278596b9426c5018f57b10623e
Eric Meldrum <eric.meldrum@gmail.com>
2019-07-28 14:58:37 -0600

    Fix bug in createList function

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ var toDoArray = [];
 
 var leftSection = document.querySelector('section');
 var mainSection = document.querySelector('main');
-var searchBar = document.querySelector('.nav__search')
+var searchBar = document.querySelector('.nav__search');
+var titleInput = document.querySelector('#task-title-input');
+var taskInput = document.querySelector('#task-item-input');
 
 leftSection.addEventListener('click', createTaskEvents)
 
-window.addEventListener('load', startOnLoad)
+// window.addEventListener('load', startOnLoad(e))
 
 function startOnLoad(e) {
 
@@ -14,21 +16,22 @@ function startOnLoad(e) {
 
 function createTaskEvents(e) {
   e.preventDefault();
-  if (e.target.id === 'task-item-input') {
-
-  };
-
+  console.log('createTaskEvents ran');
   if (e.target.id === 'make-list-btn') {
-
+    createList();
   };
+  // if (e.target.id === 'task-item-input') {
 
-  if (e.target.id === 'clear-all-btn') {
+  // };
 
-  };
 
-  if (e.target.id === 'urgency-filter-btn') {
+  // if (e.target.id === 'clear-all-btn') {
 
-  }; 
+  // };
+
+  // if (e.target.id === 'urgency-filter-btn') {
+
+  // }; 
 };
 
 function updateCardEvents(e) {
@@ -42,12 +45,43 @@ function getFromStorage() {
     toDoArray = JSON.parse(localStorage.getItem('array')).map(element => new ToDoList(element));
     sortLists();
   };
+};
 
-  function sortLists() {
+function sortLists() {
 
+};
+
+function createList(e) {
+  console.log('createList ran');
+    var newList = new ToDoList({title: titleInput.value, /*tasks: ,*/ id: Date.now(), urgent: false});
+
+    toDoArray.push(newList);
+    newList.saveToStorage(toDoArray);
+    /*sortLists();
+    removeIntro();*/
+    displayList(newList);
+};
+
+function displayList(toDoObj) {
+  console.log('displayList ran');
+  var urgent = 'images/urgent.svg';
+
+  if (toDoObj.urgent === true) {
+    urgent = 'images/urgent-active.svg'
+  } else {
+    urgent = 'images/urgent.svg'
   };
 
-  function createList() {
-    var newList = new ToDoList({})
-  }
+  mainSection.insertAdjacentHTML('afterbegin', 
+    `
+    <article class="article" data-identifier="${toDoObj.id}">
+      <p class="article__title" contenteditable="true">${toDoObj.title}</p>
+        <ul class="article__ul" id="task-list" contenteditable="true">${toDoObj.tasks}</ul> 
+      <footer class="article__footer">  
+        <img src="${urgent}" id="urgent-image" alt="lightning bolt icon">
+        <img src="images/delete.svg" id="delete-btn" alt="green x">
+      </footer>  
+    `);
 };
+
+
--- todo-list.js
+++ todo-list.js
@@ -1,24 +1,24 @@
 class ToDoList {
   constructor(toDoObj) {
     this.title = toDoObj.title;
-    this.tasks = toDoObj.tasks;
+    // this.tasks = toDoObj.tasks;
     this.id = toDoObj.id || Date.now();
     this.urgent = toDoObj.urgent || false;
-  }
+  };
 
-  saveToStorage() {
-
-  }
+  saveToStorage(gblArray) {
+    localStorage.setItem("array", JSON.stringify(gblArray));
+  };
 
   deleteFromStorage() {
 
-  }
+  };
 
   updateToDo() {
 
-  }
+  };
 
   updateTask() {
     
-  }
-}
+  };
+};
