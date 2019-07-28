b9091015b91259325999d8a1d94b4cf2d67ba394
Eric Meldrum <eric.meldrum@gmail.com>
2019-07-28 12:37:30 -0600

    Set up ToDoList class constructor and getFromStorage functions

--- main.js
+++ main.js
@@ -1,1 +1,53 @@
 var toDoArray = [];
+
+var leftSection = document.querySelector('section');
+var mainSection = document.querySelector('main');
+var searchBar = document.querySelector('.nav__search')
+
+leftSection.addEventListener('click', createTaskEvents)
+
+window.addEventListener('load', startOnLoad)
+
+function startOnLoad(e) {
+
+};
+
+function createTaskEvents(e) {
+  e.preventDefault();
+  if (e.target.id === 'task-item-input') {
+
+  };
+
+  if (e.target.id === 'make-list-btn') {
+
+  };
+
+  if (e.target.id === 'clear-all-btn') {
+
+  };
+
+  if (e.target.id === 'urgency-filter-btn') {
+
+  }; 
+};
+
+function updateCardEvents(e) {
+
+};
+
+function getFromStorage() {
+  if (JSON.parse(localStorage.getItem('array')) === null) {
+    toDoArray = [];
+  } else {
+    toDoArray = JSON.parse(localStorage.getItem('array')).map(element => new ToDoList(element));
+    sortLists();
+  };
+
+  function sortLists() {
+
+  };
+
+  function createList() {
+    var newList = new ToDoList({})
+  }
+};
--- todo-list.js
+++ todo-list.js
@@ -1,6 +1,9 @@
 class ToDoList {
-  constructor() {
-
+  constructor(toDoObj) {
+    this.title = toDoObj.title;
+    this.tasks = toDoObj.tasks;
+    this.id = toDoObj.id || Date.now();
+    this.urgent = toDoObj.urgent || false;
   }
 
   saveToStorage() {
